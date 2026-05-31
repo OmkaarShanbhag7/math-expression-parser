646bfff696baed7804b260c5ec7c941aa8d95245
OmkaarShanbhag7 <Omkaar_shanbhag@myyahoo.com>
2026-05-31 15:15:45 +0000

    parenthesis feature

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,12 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct TreeNode {
+  char data;
+  unique_ptr<TreeNode> left;
+  unique_ptr<TreeNode> right;
+};
+
 enum class TokenType{
   Number,
   Plus,
   Minus,
   Multiply,
   Divide,
+  openParen,
+  closeParen,
 };
 
 struct Token {
@@ -14,17 +22,101 @@ struct Token {
   string value;
 };
 
-struct TreeNode {
-  char data;
-  unique_ptr<TreeNode> left;
-  unique_ptr<TreeNode> right;
-};
 
 int pos = 0;
 vector<Token>tokens;
 
+vector<Token> tokenize(string input){
+
+  for(int i = 0 ; i < input.size() ;){
+    char c = input[i];
+    if(c == ' '){
+      i++;
+      continue;
+    }
+    if(isdigit(c)){
+      string numStr = "";
+      while(i < input.size() && (isdigit(input[i]) || input[i] == '.')){
+        numStr += input[i];
+        i++;
+      }
+      Token t ;
+      t.type = TokenType :: Number;
+      t.value = numStr;
+      tokens.push_back(t);
+
+      continue;
+    }
+    if(c == '+'){
+      Token t;
+      t.type = TokenType::Plus;
+      t.value = '+';
+      tokens.push_back(t);
+      i++;
+    }
+    if(c == '-'){
+      Token t;
+      t.type = TokenType::Minus;
+      t.value = '-';
+      i++;
+      tokens.push_back(t);
+
+    }
+    if(c == '*'){
+      Token t;
+      t.type = TokenType::Multiply;
+      t.value = '*';
+      i++;
+      tokens.push_back(t);
+
+    }
+    if(c == '/'){
+      Token t;
+      t.type = TokenType::Divide;
+      t.value = '/';
+      i++;
+      tokens.push_back(t);
+    }
+    if(c == '('){
+      Token t;
+      t.type = TokenType :: openParen;
+      t.value = '(';
+      i++;
+      tokens.push_back(t);
+    }
+    if(c == ')'){
+      Token t;
+      t.type = TokenType :: closeParen;
+      t.value = ')';
+      i++;
+      tokens.push_back(t);
+    }
+  }
+  return tokens;
+}
+
+unique_ptr<TreeNode> parserExpressions();
 
 unique_ptr<TreeNode> parserFactor(){
+  if(pos >= tokens.size()){
+    throw runtime_error("Syntax error : unexpected end of expression");
+  }
+  if(tokens[pos].type == TokenType::openParen){
+    pos++;
+
+    unique_ptr<TreeNode> root = parserExpressions();
+
+    if(pos >= tokens.size() || tokens[pos].type != TokenType::closeParen){
+      throw runtime_error("Syntax Error: Missing closing parenthesis ");
+    }
+    pos++;
+
+    return root;
+  }
+  if(tokens[pos].type != TokenType::Number){
+    throw runtime_error("Syntax Error: Expected a number or '(' at position : " + to_string(pos));
+  }
+ 
   Token current = tokens[pos];
 
   unique_ptr<TreeNode> node = make_unique<TreeNode>();
@@ -74,56 +166,9 @@ unique_ptr<TreeNode> parserExpressions(){
   return root;
 }
 
-
-vector<Token> tokenize(string input){
-  // vector<Token> tokens;
-
-  for(char c : input){
-    if(c == ' ') continue;
-    if(isdigit(c)){
-      Token t ;
-      t.type = TokenType :: Number;
-      t.value = string(1,c);
-
-      tokens.push_back(t);
-    }
-    if(c == '+'){
-      Token t;
-      t.type = TokenType::Plus;
-      t.value = string(1,c);
-
-      tokens.push_back(t);
-    }
-    if(c == '-'){
-      Token t;
-      t.type = TokenType::Minus;
-      t.value = string(1,c);
-
-      tokens.push_back(t);
-    }
-    if(c == '*'){
-      Token t;
-      t.type = TokenType::Multiply;
-      t.value = string(1,c);
-
-      tokens.push_back(t);
-    }
-    if(c == '/'){
-      Token t;
-      t.type = TokenType::Divide;
-      t.value = string(1,c);
-
-      tokens.push_back(t);
-    }
-  }
-  return tokens;
-}
-
-
-
 int main(){
 
-  tokenize("3 * 5 + 41 * 12");
+  tokenize("3 * ( 3 + 5 )");
   
   unique_ptr<TreeNode> treeRoot = parserExpressions();
 
